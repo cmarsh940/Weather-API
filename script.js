fd13f530befa6949b5b17f7750d4603f504582e8
cam <cam.marshall940@yahoo.com>
2017-07-01 12:46:56 -0500

    removed API #

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
 $(document).ready(function() {
     $('form').submit(function() {
         // your code here (build up your url)
-        $.get('http://api.openweathermap.org/data/2.5/weather?q='+$('input').val()+'&APPID=ff253a1779de12ff5936f745c88a7691',function(res){
+        $.get('http://api.openweathermap.org/data/2.5/weather?q='+$('input').val()+'&APPID=#',function(res){
             $('.city').html('<h1>'+res.name+'</h1>')
             var temp_f = Math.trunc((res.main.temp)*9/5-459.67)
             console.log(temp_f);
